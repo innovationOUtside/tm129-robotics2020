4baad11fafe3534ae6fffb2bd8f0a71bb7a70df4
Tony Hirst <tony.hirst@gmail.com>
2020-06-11 22:42:25 +0100

    Update gallery

--- editingWorkflowTools/imageTable.py
+++ editingWorkflowTools/imageTable.py
@@ -7,6 +7,7 @@
 # - path
 # - directory
 # - file
+# - img_path
 # - alt text
 # - image
 
@@ -47,11 +48,15 @@ rootdir = '../content'
 
 biglist = []
 for subdir, dirs, files in os.walk(rootdir):
+    if '.ipynb_checkpoints' in subdir:
+        continue
     for file in [f for f in files if f.endswith('md')]:
         fpath = os.path.join(subdir, file)
         _images = get_images_from_md(fpath)
         for (src, alt) in _images:
-            biglist.append([subdir, file, f'[{fpath}]({fpath})', alt, f'![{alt}]({os.path.join(subdir, src)})'])
+            img_path = os.path.join(subdir, src)
+            biglist.append([subdir, file, f'[{fpath}]({fpath})',
+                            img_path.replace(rootdir, ''), alt, f'![{alt}]({img_path})'])
 # -
 
 biglist[:2]
@@ -62,7 +67,5 @@ with open('gallery.md', 'w') as f:
                      tablefmt='github'))
 
 # !head gallery.md
-
-# !ls ..
 
 
